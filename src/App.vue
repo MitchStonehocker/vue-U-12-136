5ea11d4a927fb9b06b261d06a5c5f501ec56da22
MitchStonehocker <mitchstonehocker@gmail.com>
2019-11-12 15:30:37 -0500

    vue-U-12-136 custom directives

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+      <h1>Built-in Directives</h1>
+      <p>v-text</p>
+      <p v-text="'Some Text'"></p>
+      <hr>
+      <p>v-html</p>
+      <p v-html="'<strong>Some Strong Text</strong>'"></p>
+      <hr>
+    </div>
+    <hr>
+    <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+      <h1>Custom Directives</h1>
+      <p>v-highlight (defined in main.js)</p>
+      <p v-highlight:background.delayed="'red'">Color this background and text...</p>
+      <hr>
+      <p>v-local-highlight (defined in main.js)</p>
+      <p v-local-highlight="'blue'">Color this background and text...</p>
+      <hr>
+      <p>v-local-highlight (defined in main.js)</p>
+      <p v-local-highlight:background="'blue'">Color this background and text...</p>
+      <hr>
+      <p>v-local-highlight (defined in main.js)</p>
+      <p v-local-highlight:background.delayed="'blue'">Color this background and text...</p>
+      <hr>
+      <p>v-local-highlight (defined in main.js)</p>
+      <p v-local-highlight:background.delayed.blink="'blue'">Color this background and text...</p>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  directives: {
+    'local-highlight': {
+      bind (el, binding) {
+        var delay = 0
+        if (binding.modifiers['delayed']) {
+          delay = 2000
+        }
+        if (binding.modifiers['blink']) {
+          let mainColor = binding.value
+          let secondColor = 'green'
+          let currentColor = mainColor
+          setTimeout(() => {
+            setInterval(() => {
+              if (currentColor ===secondColor ? currentColor = mainColor : currentColor = secondColor)
+              if (binding.arg == 'background') {
+                el.style.backgroundColor = currentColor
+                el.style.color = 'white'
+              } else {
+                el.style.backgroundColor = 'white'
+                el.style.color = currentColor
+              }
+            }, 1000)
+          },delay)
+        } else {
+          setTimeout(() => {
+            if (binding.arg == 'background') {
+              el.style.backgroundColor = binding.value
+              el.style.color = 'white'
+            } else {
+              el.style.backgroundColor = 'white'
+              el.style.color = binding.value
+            }
+          }, delay)
+        }
+      }
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,27 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+Vue.directive('highlight', {
+  // bind (el, binding, vnode)
+  bind (el, binding) {
+    //1) el.style.backgroundColor = 'green'
+    //1) el.style.color = 'white';
+    //2) el.style.backgroundColor = binding.value
+    //2) el.style.color = 'white';
+    var delay = 0
+    if (binding.modifiers['delayed']) {
+      delay = 3000
+    }
+    setTimeout(() => {
+      if (binding.arg == 'background') {
+        el.style.backgroundColor = binding.value
+      } else {
+        el.style.color = binding.value
+      }
+    }, delay)
+  }
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
